db8e05ffea5a23d9e0717e3f53d1a89044a183c3
Sanjay Adhikari <culblank.minded@gmail.com>
2025-05-30 14:09:35 +1000

    Used jest for testing

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    testMatch: ['**/*.test.ts?(x)'],
     moduleNameMapper: {
       '\\.(css|scss|sass)$': 'identity-obj-proxy'
     }
